23d509984c46a6cc706e4e7b9a3f661e6f6fabf2
Vignesh S <41121930+vignura@users.noreply.github.com>
2018-11-02 14:29:31 +0530

    Update DrawFun.cpp

--- OpenGL/OpenGLScene/DrawFun.cpp
+++ OpenGL/OpenGLScene/DrawFun.cpp
@@ -48,7 +48,7 @@ void DrawConcCircles(float fCX, float fCY, float finRadius, float fInc, int iNof
 		fRadius += fInc;
 
 		// adjust the segment count
-		iSegments = (int) (iSegments * fRadius * (fRadius / (fRadius - fInc)));
+		//iSegments = (int) (iSegments * fRadius * (fRadius / (fRadius - fInc)));
 	}
 }
 
@@ -182,91 +182,170 @@ void DrawSphere(double dRadius, int iSlices, int iStacks)
 	gluSphere(pSphere, dRadius, iSlices, iStacks);
 }
 
+
 void DrawSolarSystem()
 {
 	float fInc = 0.25;
 	float fStartRadius = 0.5;
 	float fRadius = 0.5;
-	float fPlanetRadius = 0.05;
+	float fSunRadius = 0.25;
 	static float fTheta = 0;
+	static float fSunRadiusOffset = -SUN_RADIUS_OFFSET;
+	static int iSign = 1;
 	float foffset = (2 * PI / 5);
+	CString Msg = "";
 	
+	// sun
 	glColor3f((253 / 255.0f), (184/ 255.0f), (19 / 255.0f));
-	//glColor3f((253 / 255.0f), (10/ 255.0f), (0 / 255.0f));
-	DrawSphere(0.25, 50, 50);
+	DrawSphere(fSunRadius, 30 + fSunRadiusOffset, 30 + fSunRadiusOffset);
 	
+	
+	if(fSunRadiusOffset > SUN_RADIUS_OFFSET)
+	{
+		fSunRadiusOffset = SUN_RADIUS_OFFSET;
+		iSign *= -1;
+	}
+	else if(fSunRadiusOffset < -SUN_RADIUS_OFFSET)
+	{
+		fSunRadiusOffset = -SUN_RADIUS_OFFSET;
+		iSign *= -1;
+	}
+	else
+	{
+		fSunRadiusOffset += ((float)iSign * 0.1);
+	}
+	
+	Msg.Format("fSunRadiusOffset: %f\tiSign: %d\n", fSunRadiusOffset, iSign);
+	TRACE0(Msg);
+
 	glColor3f(0.8f, 0.8f, 0.8f);
 	DrawConcCircles(0, 0, fStartRadius, fInc, 9);
 	
 	// draw planets
+	// mercury
 	glPushMatrix();
 		glTranslatef(fRadius * cos(fTheta + foffset), fRadius * sin(fTheta + foffset), 0);
-		glColor3f((255 / 255.0f), (255/ 255.0f), (255 / 255.0f));
-		DrawSphere(fPlanetRadius, 50, 50); 
+		glColor3f(RGB_DARK_GREY);
+		//glColor3f((169 / 255.0f), (169/ 255.0f), (169 / 255.0f));
+		DrawSphere((SF_MERCURY * fSunRadius), 50, 50); 
 		fRadius += fInc;
 		foffset += foffset;
 	glPopMatrix();
 
+	// venus
 	glPushMatrix();
 		glTranslatef(fRadius * cos(fTheta + foffset), fRadius * sin(fTheta + foffset), 0);
+		glColor3f(RGB_REDDISH_BROWN);
+		DrawSphere((SF_VENUS * fSunRadius), 50, 50); 
+		fRadius += fInc;
+		foffset += foffset;
+	glPopMatrix();
+	
+	// earth
+	glPushMatrix();
+		glTranslatef(fRadius * cos(fTheta + foffset), fRadius * sin(fTheta + foffset), 0);
+		glColor3f(RGB_SKY_BLUE);
+		DrawSphere((SF_EARTH * fSunRadius), 50, 50); 
+		fRadius += fInc;
+		foffset += foffset;
+		
+		// draw moon
+		glTranslatef(0, ((SF_MOON * fSunRadius) * 10) * cos(fTheta + foffset), ((SF_MOON * fSunRadius) * 10) * sin(fTheta + foffset));
 		glColor3f((255 / 255.0f), (255/ 255.0f), (255 / 255.0f));
-		DrawSphere(fPlanetRadius, 50, 50); 
+		DrawSphere((SF_MOON * fSunRadius), 50, 50);
+
+	glPopMatrix();
+
+	// mars
+	glPushMatrix();
+		glTranslatef(fRadius * cos(fTheta + foffset), fRadius * sin(fTheta + foffset), 0);
+		glColor3f(RGB_RUST);
+		DrawSphere((SF_MARS * fSunRadius), 50, 50); 
 		fRadius += fInc;
 		foffset += foffset;
 	glPopMatrix();
 
+	// jupiter
 	glPushMatrix();
 		glTranslatef(fRadius * cos(fTheta + foffset), fRadius * sin(fTheta + foffset), 0);
-		glColor3f((255 / 255.0f), (255/ 255.0f), (255 / 255.0f));
-		DrawSphere(fPlanetRadius, 50, 50); 
+		glColor3f(RGB_LIGHT_ORANGE);
+		DrawSphere((SF_JUPITER * fSunRadius), 50, 50); 
 		fRadius += fInc;
 		foffset += foffset;
+		
+		glPushMatrix();
+			// draw moon
+			glTranslatef(0, ((SF_MOON * fSunRadius) * 10) * cos(fTheta + foffset), ((SF_MOON * fSunRadius) * 10) * sin(fTheta + foffset));
+			glColor3f((255 / 255.0f), (255/ 255.0f), (255 / 255.0f));
+			DrawSphere((SF_MOON * fSunRadius), 50, 50);
+		glPopMatrix();
+
+
+		glPushMatrix();
+			// draw moon
+			glTranslatef(0, ((SF_MOON * fSunRadius) * 14) * cos(fTheta + foffset), ((SF_MOON * fSunRadius) * 14) * sin(fTheta + foffset - 10));
+			glColor3f((255 / 255.0f), (255/ 255.0f), (255 / 255.0f));
+			DrawSphere((SF_MOON * fSunRadius), 50, 50);
+		glPopMatrix();
+
 	glPopMatrix();
 
+	// satrun
 	glPushMatrix();
 		glTranslatef(fRadius * cos(fTheta + foffset), fRadius * sin(fTheta + foffset), 0);
-		glColor3f((255 / 255.0f), (255/ 255.0f), (255 / 255.0f));
-		DrawSphere(fPlanetRadius, 50, 50); 
+		glColor3f(RGB_BROWN);
+		DrawSphere((SF_SATURN * fSunRadius), 50, 50); 
 		fRadius += fInc;
 		foffset += foffset;
+
+		// draw saturn rings
+		glColor3f(RGB_WHITE);
+		DrawConcCircles(0, 0, (SF_SATURN * fSunRadius * 1.5), (SF_SATURN * fSunRadius / 50), 20);
+
+		// draw moon
+		glTranslatef(0, ((SF_MOON * fSunRadius) * 10) * cos(fTheta + foffset), ((SF_MOON * fSunRadius) * 10) * sin(fTheta + foffset));
+		glColor3f((255 / 255.0f), (255/ 255.0f), (255 / 255.0f));
+		DrawSphere((SF_MOON * fSunRadius), 50, 50);
+
 	glPopMatrix();
 
+
+
+	// uranus
 	glPushMatrix();
 		glTranslatef(fRadius * cos(fTheta + foffset), fRadius * sin(fTheta + foffset), 0);
-		glColor3f((255 / 255.0f), (255/ 255.0f), (255 / 255.0f));
-		DrawSphere(fPlanetRadius, 50, 50); 
+		glColor3f(RGB_PALE_BLUE);
+		DrawSphere((SF_URANUS * fSunRadius), 50, 50); 
 		fRadius += fInc;
 		foffset += foffset;
-	glPopMatrix();
 
+		// draw moon
+		glTranslatef(0, ((SF_MOON * fSunRadius) * 10) * cos(fTheta + foffset), ((SF_MOON * fSunRadius) * 10) * sin(fTheta + foffset));
+		glColor3f((255 / 255.0f), (255/ 255.0f), (255 / 255.0f));
+		DrawSphere((SF_MOON * fSunRadius), 50, 50);
+
+	glPopMatrix();
+	
+	// neptune
 	glPushMatrix();
 		glTranslatef(fRadius * cos(fTheta + foffset), fRadius * sin(fTheta + foffset), 0);
-		glColor3f((255 / 255.0f), (255/ 255.0f), (255 / 255.0f));
-		DrawSphere(fPlanetRadius, 50, 50); 
+		glColor3f(RGB_AZURE_BLUE);
+		DrawSphere((SF_NEPTUNE * fSunRadius), 50, 50); 
 		fRadius += fInc;
 		foffset += foffset;
-	glPopMatrix();
+		
+		// draw moon
+		glTranslatef(0, ((SF_MOON * fSunRadius) * 10) * cos(fTheta + foffset), ((SF_MOON * fSunRadius) * 10) * sin(fTheta + foffset));
+		glColor3f((255 / 255.0f), (255/ 255.0f), (255 / 255.0f));
+		DrawSphere((SF_MOON * fSunRadius), 50, 50);
 
+	glPopMatrix();
+	
+	// pluto
 	glPushMatrix();
 		glTranslatef(fRadius * cos(fTheta + foffset), fRadius * sin(fTheta + foffset), 0);
-		glColor3f((255 / 255.0f), (255/ 255.0f), (255 / 255.0f));
-		DrawSphere(fPlanetRadius, 50, 50); 
-		fRadius += fInc;
-		foffset += foffset;
-	glPopMatrix();
-
-	glPushMatrix();
-		glTranslatef(fRadius * cos(fTheta + foffset), fRadius * sin(fTheta + foffset), 0);
-		glColor3f((255 / 255.0f), (255/ 255.0f), (255 / 255.0f));
-		DrawSphere(fPlanetRadius, 50, 50); 
-		fRadius += fInc;
-		foffset += foffset;
-	glPopMatrix();
-
-	glPushMatrix();
-		glTranslatef(fRadius * cos(fTheta + foffset), fRadius * sin(fTheta + foffset), 0);
-		glColor3f((255 / 255.0f), (255/ 255.0f), (255 / 255.0f));
-		DrawSphere(fPlanetRadius, 50, 50); 
+		glColor3f(RGB_MAROON);
+		DrawSphere((SF_PLUTO * fSunRadius), 50, 50); 
 		fRadius += fInc;
 		foffset += foffset;
 	glPopMatrix();
