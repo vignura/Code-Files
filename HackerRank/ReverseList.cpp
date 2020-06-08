acea66dba6127dfaddfd1f3c67058b33ad3c0aa0
Vignesh S <41121930+vignura@users.noreply.github.com>
2020-06-08 10:33:05 +0530

    Update ReverseList.cpp

--- HackerRank/ReverseList.cpp
+++ HackerRank/ReverseList.cpp
@@ -1,1 +1,68 @@
+#include <stdio.h>
 
+typedef struct node{
+
+	int num;
+	node *next;
+}node;
+
+void buildList(node **head, int size);
+void printList(node *head);
+void reverseList(node **head);
+
+int main()
+{
+    node *head = NULL;
+    buildList(&head, 10);
+    printList(head);
+    reverseList(&head);
+    printList(head);
+	return 0;
+}
+
+void buildList(node **head, int size)
+{
+    int i = 0;
+	node* n = NULL;
+	*head = new node;
+	n = *head;
+	
+	for(i = 0; i < (size -1); i++){
+		n->num = i;
+		n->next = new node;
+		n = n->next;
+	}
+	n->num = i;
+	n->next = NULL;
+}
+
+void printList(node *head)
+{
+	node  *n = head;
+	
+	printf("[ ");
+	while(n != NULL){
+
+		printf("%d ", n->num);
+		n = n->next;
+	}	
+	printf(" ]\n");
+}
+
+void reverseList(node **head)
+{
+    node *n = *head;
+    node *n1 = *head;
+    node *n2 = *head;
+    
+    while(n != NULL)
+    {
+        n2 = n1;
+        n1 = n;
+        n = n->next;
+        n1->next = n2;
+    }
+    n->next = n1;
+    (*head)->next = NULL;
+    *head = n;
+}
