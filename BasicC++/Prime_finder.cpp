b4c367c90f18b997bfc4d13efa7b07351970c417
Vignesh S <vignura@gmail.com>
2020-09-24 22:53:20 +0530

    updated Basic C++

--- BasicC++/Prime_finder.cpp
+++ BasicC++/Prime_finder.cpp
@@ -1,29 +1,41 @@
 #include<iostream>
 #include<cmath>
 using namespace std;
+
+bool isPrime(int);
+
 int main()
 {
-long int limit,count=1;
-cout<<"Enter the limit:";
-cin>>limit;
-bool countPrime(int);
-for(int i=2;i<limit;i++)
-{
-	if(countPrime(i))
-		count++;
+	long int limit = 0;
+	long int count = 1;
+
+	cout << "Enter the limit:";
+	cin >> limit;
+	
+	for(int i = 2; i < limit; i++)
+	{
+		if(isPrime(i)){
+			count++;
+		}
+	}
+
+	cout << "There are " << count << " no of primes in the range" << endl;
+	return 0;
 }
-cout<<"There are "<<count<<" no of primes in the range"<<endl;
-return 0;
-}
-bool countPrime(int n)
+
+bool isPrime(int n)
 {
-int lim = sqrt(n);
-int i;
-for(i=2;i<=lim;i++)
-{
-	if(n % i == 0)
-		return 0;
-}
-if(lim == i-1)
-	return 1;
+	int lim = sqrt(n);
+	int i = 2;
+
+	for(i <= lim; i++)
+	{
+		if(n % i == 0){
+			return 0;
+		}
+	}
+
+	if(lim == i-1){
+		return 1;
+	}
 }
--- BasicC++/square_root.cpp
+++ BasicC++/square_root.cpp
@@ -1,21 +1,28 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
 int main()
 {
-double sq=1,num,diff,precision=0.00001;
-cout<<"SQUARE ROOT FINDER"<<"\nenter the number:";
-cin>>num;
-int i=0;
-do{
-	sq = (sq + num/sq)/2;
-	diff = sq*sq - num;
-	if(diff < 0){
-		diff = -diff;
-		cout<<"entered if";
-	}	
-	cout<<"\n"<<sq<<"\t"<<diff;
-	i++;
-	}while((diff>precision)&&(i<30));
-cout<<"\nthe square root of "<<num<<" is "<<sq<<"\nroot * root = "<<(sq*sq)<<endl;
-return 0;
+	double sq = 1;
+	double num = 0;
+	double diff = 0;
+	double precision = 0.00001;
+
+	cout << "SQUARE ROOT FINDER" << "\nenter the number:";
+	cin >> num;
+	int i = 0;
+	do{
+		sq = ((sq + (num / sq)) / 2);
+		diff = ((sq * sq) - num);
+		if(diff < 0)
+		{
+			diff = -diff;
+			// cout << "entered if";
+		}	
+		cout << "\n" << sq << "\t" << diff;
+		i++;
+	}while((diff > precision) && (i < 30));
+
+	cout << "\nthe square root of " << num <<" is " << sq <<"\nroot * root = " << (sq*sq) << endl;
+	return 0;
 }
