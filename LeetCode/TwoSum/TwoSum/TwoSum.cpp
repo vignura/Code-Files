ac6551c7e2eba4a55e94cc2a9cf61402beaf7d21
Vignesh S <41121930+vignura@users.noreply.github.com>
2020-10-16 12:02:49 +0530

    Update TwoSum.cpp

--- LeetCode/TwoSum/TwoSum/TwoSum.cpp
+++ LeetCode/TwoSum/TwoSum/TwoSum.cpp
@@ -1,17 +1,11 @@
 // TwoSum.cpp : Defines the entry point for the console application.
 //
-
-#include "stdafx.h"
-#include <iostream>
-#include <vector>
-#include <time.h>
-#include <stdlib.h>
-#include <math.h>
-#include <unordered_map>
+#include <bits/stdc++.h>
 
 #define TEST_SIZE	30
 using namespace std;
-vector <int> input;
+typedef unordered_multimap<int, int>::iterator umit;
+
 
 class Solution {
 public:
@@ -38,48 +32,104 @@ public:
 	}
 
 #else
-	vector<int> twoSum(vector<int>& nums, int target)
-	{
-		vector<int> res;
-		vector<int> resnum;
-		unordered_map <int, int> umap;
-		int diff = 0;
-
-		/* create a unordered map with the input vector */
-		for (int i = 0; i < nums.size(); i++)
+	#if 0
+		vector<int> twoSum(vector<int>& nums, int target)
 		{
-			umap[nums[i]] = 1;			
-		}
+			vector<int> res = {0, 0};
+			unordered_map <int, int> umap;
+			int diff = 0;
 
-		/* find the two sum */
-		for (auto x: umap)
+			/* create a unordered map with the input vector */
+			for (int i = 0; i < nums.size(); i++)
+			{
+				/* store the index as the value and number as the 
+				 key, +1 is added to avoid a collision between the index 0
+				 and a out of bound value */
+				umap[nums[i]] = (i + 1);			
+			}
+
+			/* find the two sum */
+			for (auto x: umap)
+			{
+				diff = (target - x.first);
+				
+				if(umap[diff] > 0)
+				{
+					res[0] = x.second;
+					res[1] = umap[diff];
+				}
+			}
+
+			return res;
+		}
+	#else
+		vector<int> twoSum(vector<int>& nums, int target)
 		{
-			diff = (target - x.first);
-			
-			if(umap[diff] == 1)
-			{
-				resnum.push_back(x.first, diff)
-			}
-		}
+			vector<int> res = {0, 0};
+			unordered_multimap <int, int> ummap;
+			int diff = 0;
 
-		/* find the indices */
-		for (int i = 0; i < nums.size(); i++)
-		{
-			if(resnum[0] == nums[i])
+			/* create a unordered multimap with the input vector */
+			for (int i = 0; i < nums.size(); i++)
 			{
-				res.push_back(i);
+				ummap.insert(make_pair(nums[i], i));			
 			}
-			else if(resnum[1] == nums[i])
-			{
-				res.push_back(i);
-			}
-		}
 
-		return res;
-	}
+			/* find the two sum */
+			for (auto x: ummap)
+			{
+				/* compute the difference and check 
+				   wheather it is present in the map */
+				diff = (target - x.first);
+				umit it = ummap.find(diff);
+
+				if(it != ummap.end())
+				{
+					res[0] = (x.second);
+
+					if(x.second == it->second)
+					{
+						pair<umit, umit> pit = ummap.equal_range(diff);
+						pit.first++;
+						res[1] = pit.first->second;
+					}
+					else
+					{
+						res[1] = it->second;
+					}
+				}
+			}
+
+			return res;
+		}
+	#endif
 #endif
 
 };
+
+void genRandNum(vector<int> &num, int size, int limit)
+{
+	srand(time(NULL));
+	int rnum = 0;
+
+	for(int i = 0; i < size; i++)
+	{
+		rnum = (rand() % limit);
+		// rnum = i;
+		// cout << rnum << " ";
+        num.push_back(rnum);    
+	}
+}
+
+void print(vector<int> &vec)
+{
+	cout << "[";
+	for(int i = 0; i < vec.size(); i++)
+	{
+		cout << vec[i] << " ";
+	}
+	cout << "\b]\n";
+}
 
 void genTestInput(vector <int>& input, int size, int index[2], int *target)
 {
@@ -87,17 +137,8 @@ void genTestInput(vector <int>& input, int size, int index[2], int *target)
 	int n = 1;
 	int step = 0;
 
-	cout << "[";
-	for (int i = 0; i < size; i++)
-	{
-		//n = (i + step);
-		n = pow(1.5, i);
-		//n += (i + (rand() % 2));
-		input.push_back(n);
-		cout << n << " ";
-		step++;
-	}
-	cout << "]\n";
+	genRandNum(input, size, 1000);
+	print(input);
 
 	index[0] = (rand() % size);
 	if (index[0] < (size - 1))
@@ -121,10 +162,20 @@ int main()
 	Solution sol;
 	vector <int> res;
 
+#if 1
+	vector <int> input;
 	genTestInput(input, TEST_SIZE, index, &target);
+#else
+	vector <int> input = {0, 4, 3, 1};
+	index[0] = 0;
+	index[1] = 3;
+	target = 1;
+#endif
 	res = sol.twoSum(input, target);
 	
-	if (((res[0] == index[0]) || (res[0] == index[1])) &&  ((res[1] == index[0]) || (res[1] == index[1])))
+	if (((res[0] == index[0]) || (res[0] == index[1])) &&
+		((res[1] == index[0]) || (res[1] == index[1])) &&
+		(res[0] != res[1]))
 	{
 		cout << "Pass: res [" << res[0] << ", " << res[1] << "]" << endl;
 	}
@@ -133,8 +184,6 @@ int main()
 		cout << "Fail: res [" << res[0] << ", " << res[1] << "]" << endl;
 	}
 	
-	getchar();
-
 	return 0;
 }
 
