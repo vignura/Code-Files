0c77bd91b5a7efec529297ddbc7e53fd76658f1e
Vignesh S <41121930+vignura@users.noreply.github.com>
2020-10-30 18:14:00 +0530

    Update fileio.h

--- TypingSimulator/include/fileio.h
+++ TypingSimulator/include/fileio.h
@@ -1,6 +1,9 @@
 #ifndef _TYPING_SIMULATOR_FILEIO_H_
 #define _TYPING_SIMULATOR_FILEIO_H_
 
+/* refer https://stackoverflow.com/questions/13104478/uniformity-of-random-numbers-taken-modulo-n */
+#define UNIFORMITY_FACTOR	1000
+
 void readtext(const char *FileName, vector<char> &text);
 void print(vector<char> text);
 void randtext(vector<char>& text, int size);
