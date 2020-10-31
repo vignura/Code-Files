213322d8a56cdf1b5d1101ffda255a0dadb786c2
Vignesh S <41121930+vignura@users.noreply.github.com>
2020-10-31 16:43:03 +0530

    Update fileio.cpp

--- TypingSimulator/src/fileio.cpp
+++ TypingSimulator/src/fileio.cpp
@@ -76,3 +76,48 @@ void randtext(vector<char>& text, int size)
 	/* remove the trialing space */
 	text.pop_back();
 }
+
+int problemtext(vector<char>& text, int char_count, int textsize)
+{
+	int ret = 0;
+	error_db edb = {0};
+	error_stat est = {0};
+	char ch = 0;
+
+	/* read error database */
+	if(read_error_db(&edb) != TYPE_SIM_SUCCESS)
+	{
+		return TYPE_SIM_FAILURE;
+	}
+
+	/* compute error stats */
+	compute_error_stats(&edb, &est);
+
+	/* generate random text for top
+	   problem characters (ie high error count) */
+	srand(time(NULL));
+	for(int i = 1; i <= textsize; i++)
+	{
+		if((i % (CHARS_PER_WORD * WORD_PER_LINE)) == 0)
+		{
+			ch = '\n';	
+		}
+		else if((i % CHARS_PER_WORD) == 0)
+		{
+			ch = ' ';
+		}
+		else
+		{
+			/* uniformity factor is added as lower bits of rand output
+			 are not distributed uniformaly, so dividing by UNIFORMITY_FACTOR 
+			 */
+			ch = est.charset[((rand() / UNIFORMITY_FACTOR) % char_count)];
+		}
+		text.push_back(ch);
+	}
+
+	/* remove the trialing space */
+	text.pop_back();
+
+	return ret;
+}
