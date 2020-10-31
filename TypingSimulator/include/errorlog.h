91f6bbf0fb45cf45ce86a58a0de84d6e1204eeff
Vignesh S <41121930+vignura@users.noreply.github.com>
2020-10-31 16:39:42 +0530

    Update errorlog.h

--- TypingSimulator/include/errorlog.h
+++ TypingSimulator/include/errorlog.h
@@ -9,11 +9,17 @@ typedef struct{
 	unsigned long error_count[ERROR_CHAR_SET_SIZE];
 }error_db;
 
+typedef struct{
+	error_db edb;
+	char charset[ERROR_CHAR_SET_SIZE];
+}error_stat;
+
 int read_error_db(error_db* edb);
 int write_error_db(error_db* edb);
 void update_errors(error_db* edb, char error);
 void display_errors_log();
 void swap_char(char *a, char *b);
 void swap_long(unsigned long *a, unsigned long *b);
+void compute_error_stats(error_db* edb, error_stat* est);
 
 #endif /* _TYPING_SIMULATOR_ERRORLOG_H */
