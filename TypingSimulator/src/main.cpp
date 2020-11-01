c5bffd3f8cfb1389f4e3f3874b20af22fd500037
Vignesh S <vignura@gmail.com>
2020-11-01 14:38:43 +0530

    added --help to command line processing and handled all error characters

--- TypingSimulator/src/main.cpp
+++ TypingSimulator/src/main.cpp
@@ -24,9 +24,7 @@ void typing_speed_test(vector<char>& input, vector<char>& text)
 	double words  = 0;
 	double speed_wpm = 0;
 	double accuracy = 0;
-	/* added for adjusting position in output screen */
-	int xPos = 0;
-
+	
 	read_error_db(&g_srt.edb);
 	cout << "Typing speed test\n";
 	cout << "\n";
@@ -41,41 +39,22 @@ void typing_speed_test(vector<char>& input, vector<char>& text)
 	while(text.size() < input.size())
 	{
 		ch = getchar();
-		cout << ch;
 		if(ch != input[index])
 		{
-			/* on receiving a invalid new line go up
-				one line and go right xPos */
-			if(ch == '\n')
-			{
-				/* go up one line */
-				cout << "\033[1A";
-				/* go right xPos */
-				cout << "\033[" << xPos << "C";
-			}
-			else
-			{
-				update_errors(&g_srt.edb, input[index]);
-				cout << "\b";
-			}
+			update_errors(&g_srt.edb, input[index]);
 			errors++;
 		}
 		else
 		{
+			cout << ch;
 			text.push_back(ch);
 			index++;
-			xPos++;
-
-			/* reset the x position on every valid newline */
-			if(ch == '\n')	
-			{
-				xPos = 0;
-			}
 		}
 
 	}
 
 	write_error_db(&g_srt.edb);
+
 	auto stop = high_resolution_clock::now();
 	auto time = duration_cast<milliseconds>(stop - start);
 
@@ -93,9 +72,10 @@ int process_cmdline_args(int argc, const char *argv[], vector<char>& input)
 	int chars = 0;
 	char arg = 0;
 	char filename[256] = {0};
+	char option[128] = {0};
 
 	/* get the file name form the user */
-	if(argc < 3)
+	if(argc < 2)
 	{
 		printf("usage: %s [option] [parm]\n", argv[0]);
 		return TYPE_SIM_FAILURE;
@@ -107,33 +87,65 @@ int process_cmdline_args(int argc, const char *argv[], vector<char>& input)
 
 	switch(arg)
 	{
-		case 'r':
-			/* read the word count from argv */
-			iret = sscanf(argv[2], "%d", &words);
-			if(iret == 1)
+		case '-':
+			sscanf(argv[1], "--%s", option);
+			if(strncmp(option, "help", sizeof(option)) == 0)
 			{
-				randtext(input, (CHARS_PER_WORD * words));
-				iret = TYPE_SIM_SUCCESS;
+				printf("Typing simulator\n"
+					    "Test your typing speed, find your problem charcters and get better with typing simulator.\n"
+					    "Options:\n"
+					    "\t-r\tgenerate n number of random words\n"
+					    "\t-f\tuse file as input for typing test\n"
+					    "\t-d\tdisplay error stats\n"
+					    "\t-p\tgenerate n random words using the top n number of problem charcters\n");
 			}
 			else
 			{
+				printf("invalid option\n");
+			}
+
+			iret = TYPE_SIM_FAILURE;	
+		break;
+
+		case 'r':
+			if(argc < 3)
+			{
+				printf("usage: %s [option] [parm]\n", argv[0]);
 				iret = TYPE_SIM_FAILURE;
-				printf("invalid word count\n");
+			}
+			else{
+				/* read the word count from argv */
+				if(sscanf(argv[2], "%d", &words) == 1)
+				{
+					randtext(input, (CHARS_PER_WORD * words));
+					iret = TYPE_SIM_SUCCESS;
+				}
+				else
+				{
+					iret = TYPE_SIM_FAILURE;
+					printf("invalid word count\n");
+				}
 			}
 		break;
 
 		case 'f':
-			/* read the file name form the argv */
-			iret = sscanf(argv[2], "%s", filename);
-			if(iret == 1)
+			if(argc < 3)
 			{
-				readtext(filename, input);
-				iret = TYPE_SIM_SUCCESS;
-			}
-			else
-			{
+				printf("usage: %s [option] [parm]\n", argv[0]);
 				iret = TYPE_SIM_FAILURE;
-				printf("invalid filename\n");
+			}
+			else{
+				/* read the file name form the argv */
+				if(sscanf(argv[2], "%s", filename) == 1)
+				{
+					readtext(filename, input);
+					iret = TYPE_SIM_SUCCESS;
+				}
+				else
+				{
+					iret = TYPE_SIM_FAILURE;
+					printf("invalid filename\n");
+				}
 			}
 		break;
 
@@ -145,24 +157,38 @@ int process_cmdline_args(int argc, const char *argv[], vector<char>& input)
 		break;
 
 		case 'p':
-			/* generate random words with problem characters */
-			iret = sscanf(argv[2], "%d", &chars);
-			if(iret == 1)
+			if(argc < 4)
 			{
-				if(problemtext(input, chars, (12 * CHARS_PER_WORD)) == TYPE_SIM_SUCCESS)
+				printf("usage: %s [option] [parm]\n", argv[0]);
+				iret = TYPE_SIM_FAILURE;
+			}
+			else{
+				/* generate random words with problem characters */
+				if(sscanf(argv[2], "%d", &chars) == 1)
 				{
-					iret = TYPE_SIM_SUCCESS;	
+					if(sscanf(argv[3], "%d", &words) == 1)
+					{
+						if(problemtext(input, chars, (words * CHARS_PER_WORD)) == TYPE_SIM_SUCCESS)
+						{
+							iret = TYPE_SIM_SUCCESS;	
+						}
+						else
+						{
+							iret = TYPE_SIM_FAILURE;
+							printf("unable to generate problem text\n");	
+						}
+					}
+					else
+					{
+						iret = TYPE_SIM_FAILURE;
+						printf("invalid word count\n");		
+					}
 				}
 				else
 				{
 					iret = TYPE_SIM_FAILURE;
-					printf("unable to generate problem text\n");	
+					printf("invalid character count\n");
 				}
-			}
-			else
-			{
-				iret = TYPE_SIM_FAILURE;
-				printf("invalid character count\n");
 			}
 		break;
 		
