7b1a36479d9b382312e5e108cc24ddb3c3354dfc
Vignesh Sridharan <Vignesh_Sridharan@comcast.com>
2021-04-20 13:30:15 +0530

    added pragma message test

---
+++ BasicC++/pragma_message_test.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#define NAME "Vignesh"
+
+// #pragma message "Name: " NAME
+#pragma message("Name: " NAME)
+
+int main(int argc, char const *argv[])
+{
+	printf("%s\n", NAME);
+	return 0;
+}
