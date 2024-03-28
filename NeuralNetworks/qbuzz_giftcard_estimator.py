62112b4f4d1c4e03b446c1f45b4af5f0d4e16d60
Vignesh <vignura@gmail.com>
2024-03-28 09:35:33 +0530

    NeuralNetwork: Added trained models

--- NeuralNetworks/qbuzz_giftcard_estimator.py
+++ NeuralNetworks/qbuzz_giftcard_estimator.py
@@ -10,50 +10,52 @@ class giftCardEstimator (nnm.neuralNetworkManager):
     def trainModel(self, trainingDataList, epochs):
         print ("training dataset size: ", len(trainingDataList))
         self.epochs = epochs
+        # normalize data
+        normInputs = []
+        normtargets = []
+        for record in trainingDataList:
+            qbuzzUserData = record.split(',')
+            normInputs.append(self.normalizeData(qbuzzUserData[1:14]))
+            amountNormalized = float(qbuzzUserData[-1]) / 200
+            if amountNormalized < 0.01:
+                amountNormalized = 0.01
+            normtargets.append(np.zeros(self.nn.onodes) + amountNormalized)
+        
         for e in range(epochs):
             print ("training epoch:", e)
-            for record in trainingDataList:
-                qbuzzUserData = record.split(',')
-                # print (qbuzzUserData)
-                # here customer ID and gift card amount are not needed
-                inputs = self.normalizeData(qbuzzUserData[1:14])
-                # print ("normalized input data:\n", inputs)
-                # normalize gift card amount (max seems to be 200)
-                amountNormalized = float(qbuzzUserData[-1]) / 200
-                if amountNormalized < 0.01:
-                    amountNormalized = 0.01
-                targets = np.zeros(self.nn.onodes) + amountNormalized
-                # print(targets)
-                self.nn.train(inputs, targets)
+            for index in range(len(trainingDataList)):
+                self.nn.train(normInputs[index], normtargets[index])
 
     def normalizeData(self, userData):
         normalizedData = np.zeros(self.nn.inodes)
+        normDataMin = 0.2
+        normDataMax = 0.8
         # print (userData)
         # normalize signup age to 4500 days roughly 12 years
         if userData[0] != "":
             normSingupAge = float(self.conDateToAge(userData[0])) / 4500.0
         else:
             # for hole, fill the maximum value
-            normSingupAge = 0.99
+            normSingupAge = normDataMax
         normalizedData[0] = normSingupAge
         # normalize last order age to 4500 days roughly 12 years
         if userData[1] != "":
             normLastOrderAge = float(self.conDateToAge(userData[1])) / 4500.0
         else:
             # for hole fill the minimum value
-            normLastOrderAge = 0.01
+            normLastOrderAge = normDataMin
         normalizedData[1] = normLastOrderAge
         # normalize orders to 1000
         if userData[2] != "":
             normOrders = float(userData[2]) / 1000.0
         else:
-            normOrders = 0.01
+            normOrders = normDataMin
         normalizedData[2] = normOrders
         # normalize minimum first bill to 3000
         if userData[4] != "":
             normMinBill = float(userData[4]) / 3000.0
         else:
-            normMinBill = 0.01
+            normMinBill = normDataMin
         normalizedData[4] = normMinBill
         # now normalize minimum bill to 3000 as well
         if userData[3] != "":
@@ -66,48 +68,48 @@ class giftCardEstimator (nnm.neuralNetworkManager):
         if userData[5] != "":
             normReviews = float(userData[5]) / 300.0
         else:
-            normReviews = 0.01
+            normReviews = normDataMin
         normalizedData[5] = normReviews
         # normalize comments to 300 as well
         if userData[6] != "":
             normComments = float(userData[6]) / 300.0
         else:
-            normComments = 0.01
+            normComments = normDataMin
         normalizedData[6] = normComments
         # normalize images to 300
         if userData[7] != "":
             normImages = float(userData[7]) / 300.0
         else:
-            normImages = 0.01
+            normImages = normDataMin
         normalizedData[7] = normImages
         # normalize issues to 300
         if  userData [8] != "":
             normIssues = float(userData[8]) / 300.0
         else:
-            normIssues = 0.01
+            normIssues = normDataMin
         normalizedData[8] = normIssues
         # normalize orders returned to 100
         if userData[9] != "":
             normReturns = float(userData[9]) / 100.0
         else:
-            normReturns = 0.01
+            normReturns = normDataMin
         normalizedData[9] = normReturns
         # normalize category 
-        category = {"Active": 0.99, "Passive": 0.1, "Inactive": 0.01}
+        category = {"Active": normDataMax, "Passive": normDataMin, "Inactive": normDataMin}
         if userData[10] != "":
             normCategory = category[userData[10]]
         else:
             normCategory = category["Inactive"]
         normalizedData[10] = normCategory
         # normalize gender, don't add much weight to gender
-        gender = {"Male": 0.1, "Female": 0.11}
+        gender = {"Male": normDataMin, "Female": normDataMin}
         if userData[11] != "":
             normGender = gender[userData[11]]
         else:
             normGender = gender["Male"]
         normalizedData[11] = normGender
         # normalize premium
-        premium = {"Yes": 0.99, "No": 0.01}
+        premium = {"Yes": normDataMax, "No": normDataMin}
         if userData[12] != "":
             normPremium = premium[userData[12]]
         else:
@@ -162,12 +164,15 @@ class giftCardEstimator (nnm.neuralNetworkManager):
 
 # define the neural network parameter
 inputNodes = 13 # user profile parameters
-hiddenNodes = 50
+hiddenNodes = 5
 outputNodes = 1 # gift card amount
 learningRate = 0.5
 epochs = 50
 shouldSave = True
-modelFile = "NeuralNetworks/saved_networks/gift_card_estimator_nn_ip(13)_hn(50)_on(1)_lr(0.5)_ep(50).npy"
+# shouldSave = False
+modelFile = ""
+# modelFile = "NeuralNetworks/saved_networks/gift_card_estimator_nn_ip(13)_hn(50)_on(1)_lr(0.5)_ep(50).npy"
+# modelFile = "NeuralNetworks/saved_networks/gift_card_estimator_nn_ip(13)_hn(5)_on(1)_lr(0.5)_ep(100).npy"
 
 if os.path.exists(modelFile):
     # load trained model
@@ -190,6 +195,7 @@ else:
 startTime = time.time()
 scorecard = estimator.testModelCSV("NeuralNetworks/Qbuzz_dataset/test_train_2K.csv")
 print("test run time:", time.time() - startTime)
+#print("score card:", scorecard)
 scorecard_array = np.asarray(scorecard)
 print("performance:", scorecard_array.sum() / scorecard_array.size)
 
